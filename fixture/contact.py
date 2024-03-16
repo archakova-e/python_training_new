0a0eb1419cc0adc9997773d1adbf2702ec584a7b
archakova-e <161523449+archakova-e@users.noreply.github.com>
2024-03-17 00:52:45 +0300

    Правки к заданию №7: Тесты удаления, модификации для групп и контактов.

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,17 +5,16 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
-    def open_add_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
-
     def open_contact_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
 
-    def fill_contact_properties(self, contact_properties):
+    def open_add_page(self):
         wd = self.app.wd
-        self.open_add_page()
+        wd.find_element_by_link_text("add new").click()
+
+    def fill_contact_form(self, contact_properties):
+        wd = self.app.wd
         # fill contact names
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -75,77 +74,36 @@ class ContactHelper:
         wd.find_element_by_name("amonth").click()
         Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact_properties.amonth)
 
-    def modify_first_contact(self, contact_properties):
+    def select_first_contact(self):
         wd = self.app.wd
-        self.open_contact_page()
-        # modify contact names
+        wd.find_element_by_name("selected[]").click()
+
+    def edit_contact(self):
+        wd = self.app.wd
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact_properties.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact_properties.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact_properties.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact_properties.nickname)
-        # modify contact location
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact_properties.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact_properties.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact_properties.address)
-        # modify contact phones
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact_properties.home_phone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact_properties.mobile_phone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact_properties.work_phone)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact_properties.fax)
-        # modify contact emails and homepage
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact_properties.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact_properties.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact_properties.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact_properties.homepage)
-        # modify contact birthday
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact_properties.bday)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact_properties.bmonth)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact_properties.bday)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact_properties.bmonth)
-        # submit modification
+
+    def submit_modification(self):
+        wd = self.app.wd
         wd.find_element_by_name("update").click()
 
-    def delete_first_contact(self):
+    def delete_button(self):
         wd = self.app.wd
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+
+    def fill_contact_properties(self, contact_properties):
+        wd = self.app.wd
+        self.open_add_page()
+        self.fill_contact_form(contact_properties)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+
+    def modify_first_contact(self, contact_properties):
         self.open_contact_page()
-        # select first contact
-        wd.find_element_by_name("selected[]").click()
-        # select update
-        wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        # submit deletion
-        wd.find_element_by_xpath("//div[@id='content']/form[2]/input[2]").click()
+        self.edit_contact()
+        self.fill_contact_form(contact_properties)
+        self.submit_modification()
+
+    def delete_first_contact(self):
+        self.open_contact_page()
+        self.select_first_contact()
+        self.delete_button()
+
