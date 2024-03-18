a9ea79f208dc2afd162bc35a9d4b44078f1377c6
archakova-e <161523449+archakova-e@users.noreply.github.com>
2024-03-19 01:32:01 +0300

    Правки к заданию №9.

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,2 +1,6 @@
+from model.contact_properties import ContactProperties
+
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.fill_contact_properties(ContactProperties(firstname="test"))
     app.contact.delete_first_contact()
